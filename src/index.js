eebef4508cd818b1ee1978cc16457419f0319b42
Fábio Vinícius Lima <limavfabio@gmail.com>
2022-08-16 15:50:53 -0300

    Fix add button icon

--- src/index.js
+++ src/index.js
@@ -15,9 +15,7 @@ if (localStorage.getItem('tasks')) {
 }
 
 // Add add icon the addTaskBtn
-const addIcon = new Image();
-addIcon.src = './assets/plus-solid.svg';
-vars.addTaskBtn.innerHTML = `<img src="${addIcon.src}" class="add-icon">`;
+vars.addTaskBtn.innerHTML = `<i class="fas fa-plus"></i>`;
 
 
 // If a target property completed is true, then set checkbox to checked and text to strike through
